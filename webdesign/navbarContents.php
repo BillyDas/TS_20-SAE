f7196360b8a385dcf1cf6fde7efc7ef09abf0bbe
Brunius <bruinus@brunius.me>
2020-05-02 18:21:10 +1000

    Adding homepage to navbar, oops

--- webdesign/navbarContents.php
+++ webdesign/navbarContents.php
@@ -1,6 +1,7 @@
 <?php 
     echo '<div id="navbar">';
     echo '   <p>TS FSAE Data App</p>';
+	echo '   <a href=homepage.php><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Homepage</span></a>';
     echo '   <a href="custom.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Custom</span></a>';
     echo '</div>';
 ?>
