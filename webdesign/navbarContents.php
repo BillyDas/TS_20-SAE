9f027ab4bcf8c3160d51766477f2e7eaa63281b3
102119869 <102119869@student.swin.edu.au>
2020-05-02 17:14:44 +1000

    custom links to custom in navbar

--- webdesign/navbarContents.php
+++ webdesign/navbarContents.php
@@ -1,6 +1,6 @@
 <?php 
     echo '<div id="navbar">';
     echo '   <p>TS FSAE Data App</p>';
-    echo '   <a href="layout.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Custom</span></a>';
+    echo '   <a href="custom.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Custom</span></a>';
     echo '</div>';
 ?>
