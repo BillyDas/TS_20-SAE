d8614bb80fa4a0a138b9b449a4e303033f4e610e
102119869 <102119869@student.swin.edu.au>
2020-05-02 15:00:45 +1000

    Changed main page to 'custom' - multiple axes with checkboxes

--- webdesign/layout.php
+++ webdesign/layout.php
@@ -8,50 +8,59 @@
     <body>
         <div id="navbar">
             <p>TS FSAE Data App</p>
-            <?php
-                //dynamic list that takes sensors from sensordata.
-                include "dbconn.php";
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                }
-                $sql = "SELECT * FROM Sensors";
-                $result = $conn->query($sql);
-
-                if ($result->num_rows > 0)
-                {
-                    while($row = $result->fetch_assoc())
-                    {
-                        echo('<a href="layout.php?id='.$row['SensorTypeId'].'"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">'.$row["Name"].'</span></a>');
-                    }
-                }
-            ?>
+            <a href="layout.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Custom</span></a>
         </div>
         <div id="main">
-                <?php
-                    include "dbconn.php";
-                    $conn = new mysqli($servername, $username, $password, $dbname);
-                    if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                    }
-                    $sql = "SELECT * FROM Sensors WHERE SensorTypeId = " . $_GET["id"];
-                    $result = $conn->query($sql);
-
-                    if ($result->num_rows > 0)
-                    {
-                        $row = $result->fetch_assoc();
-                        echo("<h1>".$row["Name"]."</h1>");
-                    }
-                    
-                    $sql = "SELECT * FROM SensorType WHERE SensorTypeId = " . $_GET["id"];
-                    $result = $conn->query($sql);
-                    if ($result->num_rows > 0)
-                    {
-                        $row = $result->fetch_assoc();
-                        echo("<p>".$row["Description"]."</p>");
-                    }
-                ?>
+            <h1>Custom Visualisation</h1>
             <img src="cronch.png" alt="cronch" id="pic">
+            <form>
+                <fieldset>
+                    <legend>X Axis</legend>
+                    <?php
+                        //dynamic list that takes sensors from sensordata.
+                        include "dbconn.php";
+                        $conn = new mysqli($servername, $username, $password, $dbname);
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . $conn->connect_error);
+                        }
+                        $sql = "SELECT * FROM Sensors";
+                        $result = $conn->query($sql);
+
+                        if ($result->num_rows > 0)
+                        {
+                            while($row = $result->fetch_assoc())
+                            {
+                                //checkbox for each sensor follows format - (name, SensorTypeId) = id, (value) = name.
+                                echo('<input type="checkbox" id="'.$row["SensorTypeId"].'" name ="'.$row["SensorTypeId"].'>
+                                    <label for="'.$row["SensorTypeId"].'">'.$row["Name"].'</label><br>');
+                            }
+                        }
+                    ?>
+                </fieldset id="yAx">
+                <fieldset>
+                    <legend>Y Axis</legend>
+                    <?php
+                        //dynamic list that takes sensors from sensordata.
+                        include "dbconn.php";
+                        $conn = new mysqli($servername, $username, $password, $dbname);
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . $conn->connect_error);
+                        }
+                        $sql = "SELECT * FROM Sensors";
+                        $result = $conn->query($sql);
+
+                        if ($result->num_rows > 0)
+                        {
+                            while($row = $result->fetch_assoc())
+                            {
+                                //checkbox for each sensor follows format - (name, SensorTypeId) = id, (value) = name.
+                                echo('<input type="checkbox" id="'.$row["SensorTypeId"].'" name ="'.$row["SensorTypeId"].'>
+                                    <label for="'.$row["SensorTypeId"].'">'.$row["Name"].'</label><br>');
+                            }
+                        }
+                    ?>
+                </fieldset>
+            </form>
         </div>
     </body>
 </html>
