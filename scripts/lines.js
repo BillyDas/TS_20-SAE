8251c6e52c06e0d742d9d83bdf9343984db8f96e
gronges <alexanderrousseaux@gmail.com>
2020-05-05 01:48:38 +1000

    Automatic Chart Resizing and Refactoring
    Created listener event to rescale the chart on resize. Some refactoring was required, also added comments.

--- scripts/lines.js
+++ scripts/lines.js
@@ -6,78 +6,89 @@ function init() {
 	// create svg
 	var svg = d3.select(chartDiv).append("svg");
 
-	d3.json("data/SmallSensorData.json").then(function( data ) {
+	// load the dataset and then draw
+	d3.json("data/SensorData.json").then(function( data ) {
+		// data conversion for time and data
 		data.forEach( function(d) {
 			d.UTCTimestamp = Date.parse(d.UTCTimestamp);
 			d.Data = parseInt(d.Data);
 		} )
 
+		// draw line chart initially
 		lineChart( data, svg );
+
+		// add listener to draw on resize
+		window.addEventListener("resize", function() {
+				lineChart( data, svg );
+		});
 	})
 	.catch(error => {
-		console.log("Following is an error:");
+		// log the caught error if failure to load database
 		console.log(error);
 	});
 }
 
 function lineChart( data, svg ) {
+	// clear canvas
+	d3.selectAll("svg > *").remove();
+
+	// set width and height to current div environment
 	var w = chartDiv.clientWidth;
 	var h = chartDiv.clientHeight;
 
+	// set svg size
 	svg
 		.attr("width", w)
 		.attr("height", h);
 
+	// group data based on CanId
 	var sumstat = d3.nest()
 		.key(function(d) { return d.CanId; })
 		.entries(data);
 
+	// set x axis to scale based on times
 	var xScale = d3.scaleTime()
-      .domain(d3.extent(data, function(d) { return d.UTCTimestamp; }))
-      .range([padding, w - padding]);
+    	.domain(d3.extent(data, function(d) { return d.UTCTimestamp; }))
+    	.range([padding, w - padding]);
 
+    // create x axis labels
 	var xAxis = d3.axisBottom()
 		.ticks(10)
 		.scale(xScale);
 
+	// add x axis to the svg
     svg.append("g")
 		.attr("transform", "translate(0, " + (h - padding) + ")")
 		.call(xAxis);
 
+	// set y axis to scale based on data
     var yScale = d3.scaleLinear()
-				  .domain([d3.min(data, function(d) {
-				  		return d.Data;
-				  }),
-				  d3.max(data, function(d) {
-				  	return d.Data;
-				  })])
-				  .range([h - padding, padding]);
+		.domain([d3.min(data, function(d) {
+				return d.Data;
+		}),
+		d3.max(data, function(d) {
+			return d.Data;
+		})])
+		.range([h - padding, padding]);
     
+    // create y axis labels
     var yAxis = d3.axisLeft()
-				  .ticks(10)
-				  .scale(yScale);
+		.ticks(10)
+		.scale(yScale);
 
+	// add y axis to the svg
 	svg.append("g")
 		.attr("transform", "translate(" + padding + ",0)")
 		.call(yAxis);
 
-	var res = sumstat.map(function(d){ return d.key }) // list of group names
+	// create group names
+	var res = sumstat.map(function(d){ return d.key })
+	// create scale to map sensors to individual colour
 	var color = d3.scaleOrdinal()
 		.domain(res)
-		.range([
-			'#e41a1c',
-			'#377eb8',
-			'#4daf4a',
-			'#984ea3',
-			'#ff7f00',
-			'#ffff33',
-			'#a65628',
-			'#f781bf',
-			'#999999'
-		]);
+		.range(d3.schemeSet3);
 
-	//console.log(data);
-	//console.log(sumstat);
+	// add sensors as lines to the svg
 	svg.selectAll(".line")
 		.data(sumstat)
 		.enter()
@@ -88,10 +99,11 @@ function lineChart( data, svg ) {
 	        .attr("stroke-width", 1.5)
 	        .attr("d", function(d) {
 	        	return d3.line()
-	    		.x(function(d) { return xScale(d.UTCTimestamp); })
-	    		.y(function(d) { return yScale(d.Data); })
-	    		(d.values)
+		    		.x(function(d) { return xScale(d.UTCTimestamp); })
+		    		.y(function(d) { return yScale(d.Data); })
+		    		(d.values)
 	    	});
 }
 
+// run init on window load
 window.onload = init;
