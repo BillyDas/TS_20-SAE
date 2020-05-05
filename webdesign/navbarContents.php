562e5e2825be392b91cf2337c51ffe4529236bc0
Brunius <bruinus@brunius.me>
2020-05-05 13:02:51 +1000

    Navbar update

--- webdesign/navbarContents.php
+++ webdesign/navbarContents.php
@@ -1,7 +1,11 @@
 <?php 
     echo '<div id="navbar">';
     echo '   <p>TS FSAE Data App</p>';
-	echo '   <a href=homepage.php><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Homepage</span></a>';
+	echo '   <a href=homepage.php><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Home</span></a>';
+	echo '	 <hr>';
     echo '   <a href="custom.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Custom</span></a>';
+	echo '	 <hr>';
+	echo '	 <a href="faq.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">FAQ</span></a>';
+	echo '	 <a href="how2use.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Getting Started</span></a>';
     echo '</div>';
 ?>
