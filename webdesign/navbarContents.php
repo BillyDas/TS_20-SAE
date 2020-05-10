fa598392cea3ffb8d4707b47aa26a1ffa225865a
102119869 <102119869@student.swin.edu.au>
2020-05-11 06:01:02 +1000

    Added pre-baked pages

--- webdesign/navbarContents.php
+++ webdesign/navbarContents.php
@@ -1,6 +1,9 @@
 <?php 
     echo '<div id="navbar">';
     echo '   <p>TS FSAE Data App</p>';
-    echo '   <a href="custom.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Custom</span></a>';
+    echo '   <a href="custom.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Custom Visualisation</span></a>';
+    echo '   <a href="accumulators.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Accumulator Voltage</span></a>';
+    echo '   <a href="batteriesMotors.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Batteries and Motors</span></a>';
+    echo '   <a href="rpmTorque.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">RPM and Torque</span></a>';
     echo '</div>';
 ?>
