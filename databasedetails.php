d8289b73996e8064a0024d0f33d396ced022920a
Billy <Wardude202@hotmail.com>
2020-05-12 11:18:58 +1000

    Organised Files
    Organised Files

--- databasedetails.php
+++ databasedetails.php
@@ -1,9 +1,10 @@
 <?php
 //Database Login Detials
 //$host = "ts20.billydasdev.com";
-$host = "ts20.billydasdev.com";
-$user = "ts20";
+$host = "localhost";
 $pwd = "ts20";
 $sql_db = "ts20";
 $conn= @mysqli_connect($host, $user, $pwd, $sql_db);
  ?>
+
+$user = "ts20";
