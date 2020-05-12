d435e690af614dbc67e25178bd0d6f769703636f
Billy <Wardude202@hotmail.com>
2020-05-12 11:34:59 +1000

    Merge Conflicts Resolved
    Resolved merge Conflicts

--- webdesign/navbarContents.php
+++ webdesign/navbarContents.php
@@ -1,20 +1,9 @@
 <?php 
     echo '<div id="navbar">';
     echo '   <p>TS FSAE Data App</p>';
-<<<<<<< HEAD
     echo '   <a href="custom.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Custom Visualisation</span></a>';
     echo '   <a href="accumulators.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Accumulator Voltage</span></a>';
     echo '   <a href="batteriesMotors.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Batteries and Motors</span></a>';
     echo '   <a href="rpmTorque.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">RPM and Torque</span></a>';
-=======
-	echo '   <a href=homepage.php><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Home</span></a>';
-	echo '	 <hr>';
-	echo '	 <p class="category">Visualisations</p>';
-    echo '   <a href="custom.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Custom</span></a>';
-	echo '	 <hr>';
-	echo '	 <p class="category">Docs</p>';
-	echo '	 <a href="faq.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">FAQ</span></a>';
-	echo '	 <a href="how2use.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Getting Started</span></a>';
->>>>>>> BenWebDesign
     echo '</div>';
-?>
+?>
