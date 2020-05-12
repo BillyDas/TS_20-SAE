7cf92759f78d3c02b2f8c60f09d79c3a1971648c
Billy <Wardude202@hotmail.com>
2020-05-12 11:22:34 +1000

    update db details
    update db details

--- BillyDasWebDev/databasedetails.php
+++ BillyDasWebDev/databasedetails.php
@@ -1,7 +1,7 @@
 <?php
 //Database Login Detials
-//$host = "ts20.billydasdev.com";
-$host = "localhost";
+$host = "ts20.billydasdev.com";
+//$host = "localhost";
 $pwd = "ts20";
 $sql_db = "ts20";
 $conn= @mysqli_connect($host, $user, $pwd, $sql_db);
