6c07b53a43683f960b4ff2ec213f621ac606b88e
Billy <Wardude202@hotmail.com>
2020-08-17 11:17:47 +1000

    Fixed DB Conn
    Fixed Database Connection

--- BillyDasWebDev/databasedetails.php
+++ BillyDasWebDev/databasedetails.php
@@ -4,7 +4,6 @@ $host = "ts20.billydasdev.com";
 //$host = "localhost";
 $pwd = "ts20";
 $sql_db = "ts20";
+$user = "ts20";
 $conn= @mysqli_connect($host, $user, $pwd, $sql_db);
  ?>
-
-$user = "ts20";
