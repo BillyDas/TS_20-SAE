01a5cd035c8bdc8c20b9a081c7b91fb8b148d7a1
Alex <alexanderrousseaux@gmail.com>
2020-08-20 17:45:08 +1000

    GET Requests working
    Just a test

--- scripts/test.js
+++ scripts/test.js
@@ -13,6 +13,7 @@ function httpGetAsync(theUrl, callback) {
 }
 
 function init() {
+    console.log("connecting to server...");
 	const url = "http://ts20.billydasdev.com:3000/datavis?max=1";
 	fetch(url)
 		.then(response => response.json())
