db10ab2eba25f62000b3e00470e83f3980a0d943
102119869 <102119869@student.swin.edu.au>
2020-08-29 19:10:40 +1000

    Added some icons for navbar

--- webdesign/navbarContents.php
+++ webdesign/navbarContents.php
@@ -1,9 +1,9 @@
 <?php 
     echo '<div id="navbar">';
     echo '   <p>TS FSAE Data App</p>';
-    echo '   <a href="custom.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Custom Visualisation</span></a>';
-    echo '   <a href="accumulators.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Accumulator Voltage</span></a>';
-    echo '   <a href="batteriesMotors.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">Batteries and Motors</span></a>';
-    echo '   <a href="rpmTorque.php"><img src="cronch.png" alt="cronch" id="pic"><span id="inlinespan">RPM and Torque</span></a>';
+    echo '   <a href="custom.php"><img src="icons/custom.png" id="pic"><span id="inlinespan">Custom Visualisation</span></a>';
+    echo '   <a href="accumulators.php"><img src="icons/acc.png" id="pic"><span id="inlinespan">Accumulator Voltage</span></a>';
+    echo '   <a href="batteriesMotors.php"><img src="icons/engandbat.png" id="pic"><span id="inlinespan">Batteries and Motors</span></a>';
+    echo '   <a href="rpmTorque.php"><img src="icons/rpmtorque.png" id="pic"><span id="inlinespan">RPM and Torque</span></a>';
     echo '</div>';
 ?>
