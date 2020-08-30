664c617dcf195ad5c579989fb1ed69e8a16a1b1d
Bay101 <Bay101@users.noreply.github.com>
2020-08-30 11:36:38 +1000

    Fixed graph and scaling issues, began refactoring webdev folder

--- webdesign/batteriesMotors.php
+++ webdesign/batteriesMotors.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>SAE Data App</title>
-        <link rel="stylesheet" type="text/css" media="screen" href="main2.css" />
-    </head>
-    <body>
-        <?php include "navbarContents.php" ?>
-        <div id="main">
-            <h1>Battery Temperature and Motors over time</h1>
-            <img src="cronch.png" alt="cronch" id="pic">
-        </div>
-    </body>
+
+<head>
+    <title>SAE Data App</title>
+    <?php require "inc/head.html" ?>
+</head>
+
+<body>
+    <?php include "inc/navbarContents.php" ?>
+    <div id="main">
+        <h1>Battery Temperature and Motors over time</h1>
+        <img src="img/cronch.png" alt="cronch" id="pic">
+    </div>
+</body>
+
 </html>
--- webdesign/custom.php
+++ webdesign/custom.php
@@ -2,22 +2,13 @@
 <html>
 
 <head>
-    <meta charset="utf-8" />
     <title>SAE Data App</title>
-    <link rel="stylesheet" type="text/css" media="screen" href="main2.css" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
-    <link rel="stylesheet" type="text/css" href="styles/style.css">
-    <script src="https://d3js.org/d3.v5.min.js"></script>
-    <script src="scripts/loadData.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
-    <?php require "fnSensorsList.php" ?>
+    <?php require "inc/head.html" ?>
+    <?php require "func/fnSensorsList.php" ?>
 </head>
 
 <body>
-    <?php include "navbarContents.php" ?>
+    <?php include "inc/navbarContents.php" ?>
     <div id="main">
         <h1>Custom Visualisation</h1>
         <div id="graph">
@@ -29,7 +20,8 @@
             <label>X-Axis Sensors: </label>
             <select id="xAxisSelectPicker" class="selectpicker" multiple data-live-search="true">
                 <option value="" disabled>Choose x-axis sensors</option>
-                <?php echoSensors() ?>
+                <option value="Time" disabled selected>Choose x-axis sensors</option>
+                <!--<?php echoSensors() ?> -->
             </select>
             &nbsp;&nbsp;&nbsp;&nbsp;
             <label>Y-Axis Sensors: </label>
