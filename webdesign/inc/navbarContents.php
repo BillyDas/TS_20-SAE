fabc8d190678def2fe4bfb39f239d911371e596a
102119869 <102119869@student.swin.edu.au>
2020-09-02 14:38:00 +1000

    updated the home element for theme

--- webdesign/inc/navbarContents.php
+++ webdesign/inc/navbarContents.php
@@ -1,8 +1,18 @@
 <div id="navbar">
+<<<<<<< navbar
     <p>TS FSAE Data App</p>
     <a href="custom.php"><img src="img/custom.png" id="pic"><span id="inlinespan">Custom Visualisation</span></a>
     <a href="accumulators.php"><img src="img/accandvoltage.png" id="pic"><span id="inlinespan">Accumulator Voltage</span></a>
     <a href="batteriesMotors.php"><img src="img/engandbat.png" id="pic"><span id="inlinespan">Batteries and Motors</span></a>
     <a href="rpmTorque.php"><img src="img/rpmtorque.png" id="pic"><span id="inlinespan">RPM and Torque</span></a>
     <a href="addnewsensors.php"><img src="img/add.png" id="pic"><span id="inlinespan">Add a new sensor</span></a>
+=======
+    <img src="img/TS_20-ClearRed.png" id="logo"><p>TS20 FSAE<br> Data App</p>
+    <a href="index.php"><img src="img/home.png" class="pic"><span id="inlinespan">Home</span></a>
+    <a href="custom.php"><img src="img/custom.png" class="pic"><span id="inlinespan">Custom Visualisation</span></a>
+    <a href="accumulators.php"><img src="img/accandvoltage.png" class="pic"><span id="inlinespan">Accumulator Voltage</span></a>
+    <a href="batteriesMotors.php"><img src="img/engandbat.png" class="pic"><span id="inlinespan">Batteries and Motors</span></a>
+    <a href="rpmTorque.php"><img src="img/rpmtorque.png" class="pic"><span id="inlinespan">RPM and Torque</span></a>
+    <a href="addnewsensors.php"><img src="img/add.png" class="pic"><span id="inlinespan">Add a new sensor</span></a>
+>>>>>>> local
 </div>
