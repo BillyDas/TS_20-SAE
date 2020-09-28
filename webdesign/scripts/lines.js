a1345e6bb3ce7c70bc919fd352e651ec75ba1038
Bay101 <Bay101@users.noreply.github.com>
2020-09-28 10:58:31 +1000

    Fixed Missing curly braces

--- webdesign/scripts/lines.js
+++ webdesign/scripts/lines.js
@@ -27,7 +27,7 @@ function initLines() {
 
 function updateGraph() {
 	// TODO: these are subject to change, but are temporary and needed for loading img
-	if (yAxisDataId === "" || startDateTime === "" || endDateTime === "" ) // xAxis needs to be added when configured
+	if (yAxisDataId === "" || startDateTime === "" || endDateTime === "") // xAxis needs to be added when configured
 	{
 		return;
 	}
@@ -109,7 +109,7 @@ function lineChart(data, svg) {
 	let groupedSensors = groupBy(sensorDescCache, "UnitName");
 
 	var dynamicPaddingLeft = 18;
-	dynamicPaddingLeft = (groupedSensors.length+1) * dynamicPaddingLeft + 25;
+	dynamicPaddingLeft = (groupedSensors.length + 1) * dynamicPaddingLeft + 25;
 
 	// clear canvas
 	d3.selectAll("#chart svg > *").remove();
@@ -128,7 +128,7 @@ function lineChart(data, svg) {
 		.key(function (d) { return d.CanId; })
 		.entries(data);
 
-	
+
 
 	var xScale = null;
 	if (minX != null && maxX != null) {
@@ -217,30 +217,29 @@ function lineChart(data, svg) {
 
 		svg.append("text")
 			.attr("transform", "rotate(-90)")
-			.attr("y", 0 + 18*yAxisPadder)
-			.attr("x", 0 - (h / 2) + (h/2/5))
+			.attr("y", 0 + 18 * yAxisPadder)
+			.attr("x", 0 - (h / 2) + (h / 2 / 5))
 			.attr("dy", "1em")
 			.style("text-anchor", "end")
 			.style("font-size", "1em")
 			.text(groupedSensors[key][0].UnitName + " (" + groupedSensors[key][0].UnitMetric + ")")
-		
+
 		//var linesToFit = groupedSensors[key].length;
 		let keyUnitCount = 0;
-		for (let sensorsWithKeyUnit in groupedSensors[key])
-		{
+		for (let sensorsWithKeyUnit in groupedSensors[key]) {
 			// really sorry
 			svg.append("line")
 				.attr("transform", "rotate(-90)")
-				.attr("x1", 0 - (h / 2) + (h / 10) + 3 + (25*keyUnitCount))
-				.attr("y1", -5 + 18*(yAxisPadder+1))
-				.attr("x2", 0 - (h / 2) + (h / 10) + 23 + (25*keyUnitCount))
-				.attr("y2", -5 + 18*(yAxisPadder+1)) 
+				.attr("x1", 0 - (h / 2) + (h / 10) + 3 + (25 * keyUnitCount))
+				.attr("y1", -5 + 18 * (yAxisPadder + 1))
+				.attr("x2", 0 - (h / 2) + (h / 10) + 23 + (25 * keyUnitCount))
+				.attr("y2", -5 + 18 * (yAxisPadder + 1))
 				.attr("stroke", function (d) { return color(groupedSensors[key][sensorsWithKeyUnit].CanId); })
 				.attr("stroke-width", 1.5)
 
 			keyUnitCount++
 		}
-		
+
 		yAxisPadder++;
 	}
 
@@ -307,148 +306,146 @@ function focusChart(data, svg, focus) {
 	var focusHeight = 100
 	var focusPadding = 20
 
-	arc = function(d) {
+	arc = function (d) {
 		var e = +(d.type == "e"),
 			x = e ? 1 : -1,
 			y = focusHeight / 2;
-		return "M" + (.5 * x) + "," + y + "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6) + "V" + (2 * y -6) + "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y) + "Z" + "M" + (2.5 * x) + "," + (y + 8) + "V" + (2 * y - 8) + "M" + (4.5 * x) + "," + (y + 8) + "V" + (2 * y - 8);
+		return "M" + (.5 * x) + "," + y + "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6) + "V" + (2 * y - 6) + "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y) + "Z" + "M" + (2.5 * x) + "," + (y + 8) + "V" + (2 * y - 8) + "M" + (4.5 * x) + "," + (y + 8) + "V" + (2 * y - 8);
 	}
 
 	// var innerLine = d3.line()
 	// 	.length()
 
-	var brushHandle = (g, selection) => 
-	{
+	var brushHandle = (g, selection) => {
 		g.selectAll(".handle--custom")
-		.data([{type: "w"}, {type: "e"}])
-		.join(
-			enter => enter.append("path")
-				.attr("class", "handle--custom")
-				.attr("fill", "#474747")
-				.attr("fill-opacity", 0.8)
-				.attr("stroke-width", 0.5)
-				.attr("stroke", "#D9D9D9")
-				.attr("cursor", "ew-resize")
-				.attr("d", arc)
-		)
-		.attr("display", selection === null ? "none" : null)
-		.attr("transform", selection === null ? null : (d, i) => `translate(${selection[i]},${0 - focusPadding})`);
+			.data([{ type: "w" }, { type: "e" }])
+			.join(
+				enter => enter.append("path")
+					.attr("class", "handle--custom")
+					.attr("fill", "#474747")
+					.attr("fill-opacity", 0.8)
+					.attr("stroke-width", 0.5)
+					.attr("stroke", "#D9D9D9")
+					.attr("cursor", "ew-resize")
+					.attr("d", arc)
+			)
+			.attr("display", selection === null ? "none" : null)
+			.attr("transform", selection === null ? null : (d, i) => `translate(${selection[i]},${0 - focusPadding})`);
 
 
-	//var margin = { top: 20, right: 20, bottom: 30, left: 40 }
+		//var margin = { top: 20, right: 20, bottom: 30, left: 40 }
 
-	var w = chartDiv.clientWidth;
+		var w = chartDiv.clientWidth;
 
-	// group data based on CanId
-	var sumstat = d3.nest()
-		.key(function (d) { return d.CanId; })
-		.entries(data);
+		// group data based on CanId
+		var sumstat = d3.nest()
+			.key(function (d) { return d.CanId; })
+			.entries(data);
 
-	// configure focus view size
-	focus
-		.attr("viewBox", [0, 0, w, focusHeight + focusPadding])
-		.style("display", "block")
+		// configure focus view size
+		focus
+			.attr("viewBox", [0, 0, w, focusHeight + focusPadding])
+			.style("display", "block")
 
-	// focus shit
-	area = (x, y) => d3.area()
-		.defined(d => !isNaN(d.value))
-		.x(d => x(d.date))
-		.y0(y(0))
-		.y1(d => y(d.value))
+		// focus shit
+		area = (x, y) => d3.area()
+			.defined(d => !isNaN(d.value))
+			.x(d => x(d.date))
+			.y0(y(0))
+			.y1(d => y(d.value))
 
-	// set x axis to scale based on times
-	var xScale = d3.scaleTime()
-		.domain(d3.extent(data, function (d) { return d.UTCTimestamp; }))
-		.range([padding, w - padding])
-	//.nice();
+		// set x axis to scale based on times
+		var xScale = d3.scaleTime()
+			.domain(d3.extent(data, function (d) { return d.UTCTimestamp; }))
+			.range([padding, w - padding])
+		//.nice();
 
-	// create x axis labels
-	xAxisTicks = w / 100;
-	var xAxis = d3.axisBottom()
-		.ticks(w / 100)
-		.scale(xScale);
+		// create x axis labels
+		xAxisTicks = w / 100;
+		var xAxis = d3.axisBottom()
+			.ticks(w / 100)
+			.scale(xScale);
 
-	var focusYScale = d3.scaleLinear()
-		.domain([
-			d3.min(data, function (d) {
-				return d.Data;
-			}),
-			d3.max(data, function (d) {
-				return d.Data;
-			})
-		])
-		.range([focusHeight, 4])
-		.nice();
+		var focusYScale = d3.scaleLinear()
+			.domain([
+				d3.min(data, function (d) {
+					return d.Data;
+				}),
+				d3.max(data, function (d) {
+					return d.Data;
+				})
+			])
+			.range([focusHeight, 4])
+			.nice();
 
-	// create group names
-	var sensorNames = sumstat.map(function (d) { return d.key })
+		// create group names
+		var sensorNames = sumstat.map(function (d) { return d.key })
 
-	// create scale to map sensors to individual colour
-	var color = d3.scaleOrdinal()
-		.domain(sensorNames)
-		.range(d3.schemeCategory10);
+		// create scale to map sensors to individual colour
+		var color = d3.scaleOrdinal()
+			.domain(sensorNames)
+			.range(d3.schemeCategory10);
 
-	const brush = d3.brushX()
-		.extent([[padding, 0.5], [w - padding, focusHeight + 0.5]])
-		.on("brush", brushed)
-		.on("end", brushended);
+		const brush = d3.brushX()
+			.extent([[padding, 0.5], [w - padding, focusHeight + 0.5]])
+			.on("brush", brushed)
+			.on("end", brushended);
 
-	const defaultSelection = [xScale.range()[0], xScale.range()[1]];
+		const defaultSelection = [xScale.range()[0], xScale.range()[1]];
 
-	focus.append("g")
-		.attr("transform", "translate(0, " + (focusHeight + 1) + ")")
-		.call(xAxis, xScale, focusHeight);
+		focus.append("g")
+			.attr("transform", "translate(0, " + (focusHeight + 1) + ")")
+			.call(xAxis, xScale, focusHeight);
 
-	focus.selectAll(".line")
-		.data(sumstat)
-		.enter()
-		.append("path")
-		.attr("class", "line")
-		.attr("fill", "none")
-		.attr("stroke", function (d) { return color(d.key); })
-		.attr("stroke-width", 1.5)
-		.attr("d", function (d) {
-			return d3.line()
-				.x(function (d) { return xScale(d.UTCTimestamp); })
-				.y(function (d) { return focusYScale(d.Data); })
-				(d.values)
-		});
+		focus.selectAll(".line")
+			.data(sumstat)
+			.enter()
+			.append("path")
+			.attr("class", "line")
+			.attr("fill", "none")
+			.attr("stroke", function (d) { return color(d.key); })
+			.attr("stroke-width", 1.5)
+			.attr("d", function (d) {
+				return d3.line()
+					.x(function (d) { return xScale(d.UTCTimestamp); })
+					.y(function (d) { return focusYScale(d.Data); })
+					(d.values)
+			});
 
-	const gb = focus.append("g")
-		.call(brush)
+		const gb = focus.append("g")
+			.call(brush)
 
-	if (persistentSelection === null) {
-		gb.call(brush.move, defaultSelection)
-	} else {
-		gb.call(brush.move, [xScale(persistentSelection[0]), xScale(persistentSelection[1])])
-	}
-
-	function brushed() {
-		if (d3.event.selection) {
-			//focus.property("value", d3.event.selection.map(xScale.invert, xScale));
-			focus.dispatch("input");
-
-			[minX, maxX] = d3.event.selection.map(xScale.invert, xScale)
-			minY = d3.min(data, d => minX <= d.UTCTimestamp && d.UTCTimestamp <= maxX ? d.value : NaN);
-			maxY = d3.max(data, d => minX <= d.UTCTimestamp && d.UTCTimestamp <= maxX ? d.value : NaN);
-
-			lineChart(data, svg)
-
-			d3.select(this).call(brushHandle, d3.event.selection);
+		if (persistentSelection === null) {
+			gb.call(brush.move, defaultSelection)
+		} else {
+			gb.call(brush.move, [xScale(persistentSelection[0]), xScale(persistentSelection[1])])
 		}
 	}
+}
 
-	function brushended() {
-		if (!d3.event.selection) {
-			gb.call(brush.move, defaultSelection);
-		}
-		else {
-			// can even move to brushed(), just probably better performance here?
-			persistentSelection = [xScale.invert(d3.event.selection[0]), xScale.invert(d3.event.selection[1])];
-		}
+function brushed() {
+	if (d3.event.selection) {
+		//focus.property("value", d3.event.selection.map(xScale.invert, xScale));
+		focus.dispatch("input");
+
+		[minX, maxX] = d3.event.selection.map(xScale.invert, xScale)
+		minY = d3.min(data, d => minX <= d.UTCTimestamp && d.UTCTimestamp <= maxX ? d.value : NaN);
+		maxY = d3.max(data, d => minX <= d.UTCTimestamp && d.UTCTimestamp <= maxX ? d.value : NaN);
+
+		lineChart(data, svg)
+
+		d3.select(this).call(brushHandle, d3.event.selection);
 	}
+}
 
-
+function brushended() {
+	if (!d3.event.selection) {
+		gb.call(brush.move, defaultSelection);
+	}
+	else {
+		// can even move to brushed(), just probably better performance here?
+		persistentSelection = [xScale.invert(d3.event.selection[0]), xScale.invert(d3.event.selection[1])];
+	}
 }
 
 // run init on window load
