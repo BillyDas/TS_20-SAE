6faf531645e9aa219261e70264347b259b0ac044
Bay101 <baylin.ferguson@gmail.com>
2020-10-06 09:28:48 +1100

    dynamic hostnames
    changed the code to utilise dynamic host names rather than static to allow for the docker deployment to work on any server

--- webdesign/func/exportCSV.php
+++ webdesign/func/exportCSV.php
@@ -1,14 +1,18 @@
 <?php
-    $connect = mysqli_connect("ts20.billydasdev.com", "ts20", "ts20", "ts20");
-    header('Content-Type: text/csv; charset=utf-8');
-    header('Content-Disposition: attachment; filename=RaceCarData.csv');
-    $output = fopen("php://output", "w");
-    fputcsv($output, array('SensorDataId', 'CanId', 'Data', 'UTCTimestamp'));
-    $query = "SELECT * from SensorData";
-    $result = mysqli_query($connect, $query);
-    while($row = mysqli_fetch_assoc($result))
-    {
-      fputcsv($output, $row);
-    }
-    fclose($output);
+  require_once('dbconn.php');
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }  
+  header('Content-Type: text/csv; charset=utf-8');
+  header('Content-Disposition: attachment; filename=RaceCarData.csv');
+  $output = fopen("php://output", "w");
+  fputcsv($output, array('SensorDataId', 'CanId', 'Data', 'UTCTimestamp'));
+  $query = "SELECT * from SensorData";
+  $result = mysqli_query($conn, $query);
+  while($row = mysqli_fetch_assoc($result))
+  {
+    fputcsv($output, $row);
+  }
+  fclose($output);
 ?>
